1090d2e085c56d35deb44b75d0dfad7f1170ebbe
Curtis Lewis <culewis@pdx.edu>
2019-11-07 11:38:56 -0800

    Excercise1 Done

---
+++ week5/excercise1/excercise1.js
@@ -0,0 +1,16 @@
+function oneToOneHundred() {
+  const fun = () => {
+    for (i = 1; i <= 100; ++i) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz");
+      } else if (i % 3 == 0) {
+        console.log("fizz");
+      } else if (i % 5 == 0) {
+        console.log("buzz");
+      } else {
+        console.log(i);
+      }
+    }
+  };
+  return fun;
+}
