cade9ae6af5236d8f4247e2f8c22434efdea3cd8
Curtis Lewis <culewis@pdx.edu>
2019-11-18 13:00:34 -0800

    Finished with week6 assignment.

--- week6/app.js
+++ week6/app.js
@@ -1,6 +1,9 @@
 var path = require('path');
 var express = require('express');
+
 var app = express();
+console.log("Express app initialized");
+
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
@@ -11,16 +14,27 @@ const PORT = 8080
 
 
 //main route fget request/....
-app.get("/", function(req,res){
+app.get('/', function(req,res){
   console.log("index page");
 
-  //res.sendFile(path.join(__dirname, ))
-}
+  res.sendFile(path.join(__dirname, 'index.html'))
+});
+
 
 
 app.post('/submit', function (req, res) {
   console.log("submit page");
-  res.send(req.body);
-});
+  //res.send(req.body);
+
+  let name = 'Name: ' + req.body.name;
+  let email = 'Email: ' + req.body.email;
+  let message = 'Comment: ' + req.body.message;
+
+  res.write(name + '\r\n');   
+  res.write(email + '\r\n');
+  res.write(message + '\r\n');
+ 
+  res.end();
+ });
 
 app.listen(PORT);
---
+++ week6/readme.txt
@@ -0,0 +1,1 @@
+To run, launch app.js and go to http://localhost:8080/
